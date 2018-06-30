6e4aa891cafe5dc966f01f8b7e8e613fe88af0cb
alex0112 <alarsen0112@gmail.com>
2018-06-29 18:43:06 -0600

    routing and config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   scope '/api' do
-    resources :trees, only: [:show]
+    resources :trees, only: [:show], param: :root_uri, constraints: { root_uri: /[^\/]+/ }
   end
 end
