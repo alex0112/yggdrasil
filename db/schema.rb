1291b87c744c93c89aca0c823ec1564028b48111
alex0112 <alarsen0112@gmail.com>
2018-06-29 18:42:52 -0600

    The db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180616042121) do
+ActiveRecord::Schema.define(version: 20180627041225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180616042121) do
     t.json "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "root_uri"
   end
 
 end
