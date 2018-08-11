a745298db5a551c476885fe057bd4d6596ac56dc
alex0112 <alarsen0112@gmail.com>
2018-08-11 11:07:21 -0600

    Added searchbar component

---
+++ client/src/components/searchbar.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+
+    inputHandler(input) {
+	
+    }
+
+    render() {
+	return(
+	    <div id="searchbar">
+	      <label>URI: </label>
+	      <input type="url" name="url" id="url"
+		     placeholder="https://"
+		     pattern="https://.*" size="20" required
+		     />
+	    </div>
+
+	);
+
+    }
+
+}
