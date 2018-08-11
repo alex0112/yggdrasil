ebb433759eb27c8fdff6d1fc3c021866e9d9f905
alex0112 <alarsen0112@gmail.com>
2018-08-11 16:54:40 -0600

    baseURL is now points to localhost:3000 only when NODE_ENV is development

--- client/src/datalayer/datalayer.js
+++ client/src/datalayer/datalayer.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
+const api = process.env.NODE_ENV === 'development' ? axios.create({baseURL: 'http://localhost:3000/'}) : axios.create();
 
 export function getTreeData(uri) {
-    axios.get(`/api/trees/${uri}`)
+    api.get(`/api/trees/${uri}`)
 	.then(function (response) {
-	    console.log(response);
+	    console.log(response.data);
 	})
 	.catch(function (error) {
 	    console.log(error);
