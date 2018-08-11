bc087ea08ae9af34164d5b1f3a1a4b5c5ced688c
alex0112 <alarsen0112@gmail.com>
2018-08-11 14:23:56 -0600

    More permissive CORS policy in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Permissive CORS policy in development:
+  config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+  }
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
