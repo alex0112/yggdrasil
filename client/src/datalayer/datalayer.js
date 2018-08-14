70f18f175f01a60205785f12be66ccfc0f22f0ba
alex0112 <alarsen0112@gmail.com>
2018-08-13 18:45:49 -0600

    Pushing up so Prachi can help me review

--- client/src/datalayer/datalayer.js
+++ client/src/datalayer/datalayer.js
@@ -3,10 +3,12 @@ const api = process.env.NODE_ENV === 'development' ? axios.create({baseURL: 'htt
 
 export function getTreeData(uri) {
     api.get(`/api/trees/${uri}`)
-	.then(function (response) {
-	    console.log(response.data);
-	})
-	.catch(function (error) {
-	    console.log(error);
-	});
+	     .then(function (response) {
+		 console.log("In the promise:");
+		 console.log(response.data);
+		 return response.data;
+	     })
+	     .catch(function (error) {
+		 console.log(error);
+	     });
 }
