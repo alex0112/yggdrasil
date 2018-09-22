e2a8061787dac47dd1d00bc152643186af99d200
alex0112 <alarsen0112@gmail.com>
2018-09-22 08:04:35 -0700

    Searchbar is now a form to take advantage of HTML5 features

--- client/src/components/searchbar.jsx
+++ client/src/components/searchbar.jsx
@@ -7,12 +7,14 @@ export default class SearchBar extends Component {
     render() {
 	return(
 	    <div id="searchbar">
-	      <input type="url" className="wiki-input" name="url" id="url"
-		     placeholder="https://"
-		     pattern="https://.*" required
-		     onChange={ (e) => this.setState({ input: e.target.value}) }
-		     />
-		<input type="submit" className="wiki-button" onClick={ () => { this.props.clickHandler(this.state.input) }} />
+	      <form onSubmit={ () => { this.props.clickHandler(this.state.input) } } >
+		<input type="url" className="wiki-input" name="url" id="url"
+		       placeholder="https://"
+		       pattern="https://.*"
+		       onChange={ (e) => this.setState({ input: e.target.value}) }
+		  />
+		  <input type="submit" className="wiki-button" onClick={ () => { this.props.clickHandler(this.state.input) } } />
+	      </form>
 	    </div>
 	);
     }
