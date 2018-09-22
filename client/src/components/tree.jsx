3b68bdcd851d641ab58d4e2d4565f3b4337bfa99
alex0112 <alarsen0112@gmail.com>
2018-09-22 07:24:17 -0700

    Added comments

--- client/src/components/tree.jsx
+++ client/src/components/tree.jsx
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 import Tree from 'react-d3-tree';
 import Loader from './loader';
 
-// const myTreeData = [
+//  <Tree /> expects data in this form:
+// [
 //   {
 //     name: 'Top Level',
 //     children: [
@@ -17,17 +18,19 @@ import Loader from './loader';
 //   },
 // ];
 
-export default class Yggdrasil extends Component {
+export default class Yggdrasil extends Component {  // TODO:  Functional component.
 
     render() {
 	return (
 	    <div id='container'>
-	      {this.props.data.length === 0 ? <Loader />
-		  : <Tree
-			  data={this.props.data}
-			  orientation='vertical'
-			  /> }
+	      {this.props.data.length !== 0 ?
+		  <Tree
+			data={this.props.data}
+			orientation='vertical'
+			/>
+		  : "" }
 	    </div>
 	    );
     }
+
 }
