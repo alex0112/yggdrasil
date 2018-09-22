cff91dcbcc3e8162716f836e944b49699c535dcc
alex0112 <alarsen0112@gmail.com>
2018-09-22 07:14:05 -0700

    Poor man's spinner

--- client/src/components/tree.jsx
+++ client/src/components/tree.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 
 import Tree from 'react-d3-tree';
+import Loader from './loader';
 
 // const myTreeData = [
 //   {
@@ -21,7 +22,7 @@ export default class Yggdrasil extends Component {
     render() {
 	return (
 	    <div id='container'>
-	      {this.props.data.length === 0 ? 'Loading...'
+	      {this.props.data.length === 0 ? <Loader />
 		  : <Tree
 			  data={this.props.data}
 			  orientation='vertical'
