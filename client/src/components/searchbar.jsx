e6030ab218b0ef78f26e96eca845a50094fa232e
alex0112 <alarsen0112@gmail.com>
2019-08-07 23:11:33 -0600

    Enter key now submits searchbar.  Closes #10

--- client/src/components/searchbar.jsx
+++ client/src/components/searchbar.jsx
@@ -13,7 +13,12 @@ export default class SearchBar extends Component {
 		       placeholder="https://"
 		       pattern="https://.*"
 		       onChange={ (e) => this.setState({ input: e.target.value}) }
-		  />
+          onKeyUp={ (e) => {
+              if (e.which === 13) { // if the Enter key is pressed
+                  this.props.clickHandler(this.state.input);
+              }
+          } }
+          />
 		  <input type="submit" className="wiki-button" onClick={ () => { this.props.clickHandler(this.state.input) } } />
 
 	    </div>
